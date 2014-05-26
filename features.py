6f26727cac0562bf8d300a747fbab870d1f58a72
Ian Tenney <iftenney@stanford.edu>
2014-05-25 21:51:01 -0700

    Added stemming (using PorterStemmer), and entropy feature.

--- features.py
+++ features.py
@@ -17,12 +17,15 @@ import time
 import nltk
 import hyphen
 
-from numpy import sqrt
+from numpy import linalg
+from numpy import array, sqrt, log
 
 ###########################
 # Static Helper Functions #
 ###########################
 
+##
+# Readability scores
 def is_polysyllabic(w, hyphenator=hyphen.Hyphenator('en_US')):
     if len(w) > 30: return False
     return (len(hyphenator.syllables(unicode(w))) >= 3)
@@ -36,6 +39,41 @@ def SMOG(words, sentences):
     grade = 1.0430 * sqrt(val) + 3.1291
     return grade
 
+##
+# Distributional models
+def counter_dot_product(c1,c2, norm=False):
+    """Compute the dot product of two sparse vectors (as dicts).
+    If norm=True, then will normalize each before computing,
+    to yield the cosine distance."""
+    c1_f = {k:v for k,v in c1.items() if k in c2}
+    c2_f = {k:v for k,v in c1.items() if k in c1}
+    dp = sum([c1_f[k]*c2_f[k] for k in c1_f.keys()])
+    if norm == True:
+        nc = linalg.norm(c1.values()) * linalg.norm(c2.values())
+    else: nc = 1.0
+    return dp/nc
+
+
+def counter_L1_norm(c):
+    """Normalize a counter by sum of elements, 
+    i.e. to convert to a probability distribution."""
+    total = sum(c.values())
+    return {k:(v*1.0/total) for k,v in c.items()}
+
+def counter_entropy(c):
+    """Basic entropy, treating counter as a 
+    fully-specified multinomial distribution."""
+    cn = array(counter_to_distribution(c).values())
+    return -1*sum(cn * log(cn))
+
+def counter_entropy_normalized(c):
+    """
+    Calculate the entropy of a word counter.
+    Normalized by total word count, as in Hsu, et al.
+    """
+    wordcount = 1.0*sum(c.values())
+    cn = array(c.values())/wordcount
+    return (-1/wordcount)*(sum(cn * log(cn)) - log(wordcount))
 
 ####################
 # FeatureSet Class #
@@ -66,6 +104,7 @@ class FeatureSet(object):
                     'SMOG',
                     'n_verbs',
                     'n_nouns',
+                    'entropy',
                     ]
     
     # To-Do: add distributional features
@@ -149,6 +188,10 @@ class FeatureSet(object):
         self.SMOG = SMOG(self.wordCounts, self.sentences)
 
 
+    ##
+    # Part-of-Speech (POS) tagging, and associated features
+    # - n_nouns
+    # - n_verbs
     def pos_tag(self, tagger=nltk.tag.pos_tag):
         """Calculate part-of-speech tags."""
         self.posTags = map(tagger, self.words)
@@ -159,3 +202,16 @@ class FeatureSet(object):
                             if t.startswith("NN")])
         self.n_verbs = len([w for w,t in itertools.chain(*self.posTags)
                             if t.startswith("VB")])
+
+
+    ##
+    # Stemming, and distributional features
+    # (requires: tokenize())
+    # - entropy
+    #
+    def stem(self, stemmer=nltk.stem.porter.PorterStemmer()):
+        self.stemCounts = {stemmer.stem(k):v for k,v in self.wordCounts.items()}
+
+    def calc_entropy(self):
+        """Calculate entropy from stemmed word distribution."""
+        self.entropy = counter_entropy_normalized(self.stemCounts)
