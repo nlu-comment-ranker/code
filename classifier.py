a4b8ef46603e4e2014d2f12f9b2301bea3886217
Sammy Nguyen <smnguyen@stanford.edu>
2014-06-08 00:24:05 -0700

    10-90 train-test split

--- classifier.py
+++ classifier.py
@@ -4,10 +4,12 @@ from sklearn.svm import SVR
 from sklearn.cross_validation import train_test_split
 from sklearn.cross_validation import KFold
 from sklearn import preprocessing
+from sklearn.metrics import mean_squared_error
 import pandas as pd
 import numpy as np
 import math
 import time
+import random
 
 
 # Removes rows with NaN or inf in specified columns
@@ -20,8 +22,12 @@ def clean_data(data, columns):
 # Given Pandas dataframe, returns 80/20 split into dev and test sets
 # Usage:
 #   dev_data, test_data = split_data(data)
-def split_data(data):
-    dev_sid, eval_sid = train_test_split(data.sid.unique(), test_size=0.2, random_state=42)
+def split_data(data, limit_data=False):
+    if limit_data:
+        random.seed(42)
+        sids = random.sample(data.sid.unique(), 1000)
+        data = data[data.sid.map(lambda x: x in sids)]
+    dev_sid, eval_sid = train_test_split(data.sid.unique(), test_size=0.9, random_state=42)
     dev_df = data[data.sid.map(lambda x: x in dev_sid)]
     eval_df = data[data.sid.map(lambda x: x in eval_sid)]
     return dev_df, eval_df
@@ -58,9 +64,14 @@ def _dcg(scores, k):
 
 def ndcg(data, k):
     scores = np.zeros(k)
+    skipped_submissions = 0
     for sid in data.sid.unique():
         # Add ranks and favorability scores to data frame (Hsu et al.)
         comments = data[data.sid == sid]
+        if len(comments) == 0:
+            skipped_submissions += 1
+            continue
+
         ranks = range(1, len(comments) + 1)
         comments = comments.sort('pred', ascending=False)
         comments['pred_rank'] = ranks
@@ -73,7 +84,7 @@ def ndcg(data, k):
         idcgs = _dcg(comments.fav, k)
         scores += (dcgs / idcgs)
 
-    return scores / float(len(data.sid.unique()))
+    return scores / (len(data.sid.unique()) - skipped_submissions)
 
 
 if __name__ == '__main__':
@@ -82,17 +93,19 @@ if __name__ == '__main__':
     parser.add_argument('-f', '--features', type=str, nargs='+', help='Features list')
     parser.add_argument('-l', '--list-features', action='store_true', default=False,
                         help='Show possible features', dest='list_features')
+    parser.add_argument('-L', '--limit-data', action='store_true', default=False,
+                        help='Limit to 1000 submissions', dest='limit_data')
     args = parser.parse_args()
 
     data = pd.read_hdf(args.datafile, 'data')
     print 'Original data dims: ' + str(data.shape)
     if args.list_features:
-        print ' '.join(data.columns.values)
+        print '\n'.join(data.columns.values)
         exit(0)
     columns = args.features
     data = clean_data(data, columns)
     print 'Cleaned data dims: ' + str(data.shape)
-    train_df, test_df = split_data(data)
+    train_df, test_df = split_data(data, args.limit_data)
     train_mat = train_df.filter(columns + ['cid', 'sid', 'score']).as_matrix()
     test_mat = test_df.filter(columns + ['cid', 'sid', 'score']).as_matrix()
 
@@ -121,6 +134,7 @@ if __name__ == '__main__':
     print 'Performance on training data'
     for i, score in enumerate(ndcg(data_pred, 20), start=1):
         print '\tNDCG@%d: %.5f' % (i, score) 
+    print 'Karma MSE: %.5f' % mean_squared_error(train_mat[:, -1], train_pred)
 
     test_pred = svr.predict(test_data)
     data_pred = pd.DataFrame(np.column_stack((test_mat[:, -3:], test_pred)), 
@@ -128,3 +142,4 @@ if __name__ == '__main__':
     print 'Performance on test data'
     for i, score in enumerate(ndcg(data_pred, 20), start=1):
         print '\tNDCG@%d: %.5f' % (i, score) 
+    print 'Karma MSE: %.5f' % mean_squared_error(test_mat[:, -1], test_pred)
