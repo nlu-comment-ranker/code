ad18fc86765a07f818aa3c37e6a9f43ba04c39e0
Ian Tenney <iftenney@stanford.edu>
2014-06-08 19:03:11 -0700

    Rewrote a portion of the code to use cleaner DataFrame operations, and allow easier output at end.

--- classifier.py
+++ classifier.py
@@ -1,3 +1,15 @@
+##
+# Social Web Comment Ranking
+# CS224U Spring 2014
+# Stanford University 
+#
+# Classifier Engine
+#
+# Sammy Nguyen
+# Ian Tenney
+# June 2, 2014
+##
+
 from argparse import ArgumentParser
 from sklearn.grid_search import GridSearchCV
 from sklearn.svm import SVR
@@ -21,20 +33,24 @@ def clean_data(data, columns):
 
 # Given Pandas dataframe, returns 80/20 split into dev and test sets
 # Usage:
-#   dev_data, test_data = split_data(data)
-def split_data(data, limit_data=False):
-    if limit_data:
+#   dev_data, test_X = split_data(data)
+def split_data(data, limit_data=0, test_fraction=0.9):
+    if limit_data > 0:
         random.seed(42)
-        sids = random.sample(data.sid.unique(), 1000)
+        sids = random.sample(data.sid.unique(), limit_data)
         data = data[data.sid.map(lambda x: x in sids)]
-    dev_sid, eval_sid = train_test_split(data.sid.unique(), test_size=0.9, random_state=42)
+
+    # Split along unique submission IDs
+    dev_sid, eval_sid = train_test_split(data.sid.unique(), 
+                                         test_size=test_fraction, 
+                                         random_state=42)
     dev_df = data[data.sid.map(lambda x: x in dev_sid)]
     eval_df = data[data.sid.map(lambda x: x in eval_sid)]
     return dev_df, eval_df
 
 
 # Runs grid search to determine the best parameters to use for SVR
-def train_optimal_classifier(train_data, train_target):
+def train_optimal_classifier(train_data, train_y):
     parameters = {
         'kernel': ['rbf'], 
         'C': [0.1, 1, 10, 100, 500],
@@ -43,15 +59,18 @@ def train_optimal_classifier(train_data, train_target):
         'epsilon': [0.1],
         'tol': [1e-1]}
     svr = SVR()
-    cv_split = KFold(len(train_target), n_folds=10, random_state=42)
+    cv_split = KFold(len(train_y), n_folds=10, random_state=42)
     grid_search = GridSearchCV(svr, parameters, cv=cv_split, n_jobs=8)
-    grid_search.fit(train_data, train_target)
+    grid_search.fit(train_data, train_y)
 
     params = grid_search.best_params_ 
     estimator = grid_search.best_estimator_
     print 'Number of support vectors: %d' % len(estimator.support_vectors_)
     return params, estimator
 
+######################
+# Evaluation Metrics #
+######################
 
 def _dcg(scores, k):
     if len(scores) > k:
@@ -87,59 +106,111 @@ def ndcg(data, k):
     return scores / (len(data.sid.unique()) - skipped_submissions)
 
 
-if __name__ == '__main__':
-    parser = ArgumentParser(description='Run SVR')
-    parser.add_argument('datafile', type=str, help='HDF5 data file')
-    parser.add_argument('-f', '--features', type=str, nargs='+', help='Features list')
-    parser.add_argument('-l', '--list-features', action='store_true', default=False,
-                        help='Show possible features', dest='list_features')
-    parser.add_argument('-L', '--limit-data', action='store_true', default=False,
-                        help='Limit to 1000 submissions', dest='limit_data')
-    args = parser.parse_args()
-
+def main(args):
+    # Load Data File
     data = pd.read_hdf(args.datafile, 'data')
+    
     print 'Original data dims: ' + str(data.shape)
     if args.list_features:
         print '\n'.join(data.columns.values)
         exit(0)
-    columns = args.features
-    data = clean_data(data, columns)
+    
+    # Select Features and trim data so all features present
+    feature_names = args.features
+    data = clean_data(data, feature_names)
     print 'Cleaned data dims: ' + str(data.shape)
-    train_df, test_df = split_data(data, args.limit_data)
-    train_mat = train_df.filter(columns + ['cid', 'sid', 'score']).as_matrix()
-    test_mat = test_df.filter(columns + ['cid', 'sid', 'score']).as_matrix()
 
-    print "Training set: %d examples" % (train_mat.shape[0],)
-    print "Test set: %d examples" % (test_mat.shape[0],)
-    print "Selected %d features" % (len(columns),)
-    print 'Features: ' + ' '.join(columns)
+    # Split into train, test
+    # and select training target
+    target = args.target
+    train_df, test_df = split_data(data, args.limit_data,
+                                   args.test_fraction)
 
-    train_data = train_mat[:, :-3].astype(np.float)
-    train_target = train_mat[:, -1].astype(np.float)
-    test_data = test_mat[:, :-3].astype(np.float)
-    test_target = test_mat[:, -1].astype(np.float)
+    # Split into X, y for regression
+    train_X = train_df.filter(feature_names).as_matrix().astype(np.float) # training data
+    train_y = train_df.filter([target]).as_matrix().astype(np.float) # training labels
+    test_X = test_df.filter(feature_names).as_matrix().astype(np.float) # test data
+    test_y = test_df.filter([target]).as_matrix().astype(np.float) # ground truth
 
-    scaler = preprocessing.StandardScaler().fit(train_data)
-    train_data = scaler.transform(train_data)
-    test_data = scaler.transform(test_data)
+    # import pdb
+    # pdb.set_trace()
 
+    # For compatibility, make 1D
+    train_y = train_y.reshape((-1,))
+    test_y = test_y.reshape((-1,))
+
+    print "Training set: %d examples" % (train_X.shape[0],)
+    print "Test set: %d examples" % (test_X.shape[0],)
+    print "Selected %d features" % (len(feature_names),)
+    print 'Features: %s' % (' '.join(feature_names))
+
+    ##
+    # Preprocessing: scale data, keep SVM happy
+    scaler = preprocessing.StandardScaler()
+    train_X = scaler.fit_transform(train_X) # faster than fit, transform separately
+    test_X = scaler.transform(test_X)
+
+    ##
+    # Run Grid Search / 10xv on training/dev set
     start = time.time()
-    params, svr = train_optimal_classifier(train_data, train_target)
+    params, svr = train_optimal_classifier(train_X, train_y)
     print params
     print "Took %.2f minutes to train" % ((time.time() - start) / 60.0)
 
-    train_pred = svr.predict(train_data)
-    data_pred = pd.DataFrame(np.column_stack((train_mat[:, -3:], train_pred)),
-                             columns=['cid', 'sid', 'score', 'pred'])
-    print 'Performance on training data'
-    for i, score in enumerate(ndcg(data_pred, 20), start=1):
-        print '\tNDCG@%d: %.5f' % (i, score) 
-    print 'Karma MSE: %.5f' % mean_squared_error(train_mat[:, -1], train_pred)
+    ##
+    # Predict scores for training set
+    result_label = "pred_%s" % target # e.g. pred_score
+    train_pred = svr.predict(train_X)
+    train_df[result_label] = train_pred
 
-    test_pred = svr.predict(test_data)
-    data_pred = pd.DataFrame(np.column_stack((test_mat[:, -3:], test_pred)), 
-                             columns=['cid', 'sid', 'score', 'pred'])
+    print 'Performance on training data'
+    # for i, score in enumerate(ndcg(data_pred, 20), start=1):
+    #     print '\tNDCG@%d: %.5f' % (i, score) 
+    print 'Karma MSE: %.5f' % mean_squared_error(train_y, train_pred)
+
+    ##
+    # Predict scores for test set
+    test_pred = svr.predict(test_X)
+    test_df[result_label] = test_pred
+
     print 'Performance on test data'
-    for i, score in enumerate(ndcg(data_pred, 20), start=1):
-        print '\tNDCG@%d: %.5f' % (i, score) 
-    print 'Karma MSE: %.5f' % mean_squared_error(test_mat[:, -1], test_pred)
+    # for i, score in enumerate(ndcg(data_pred, 20), start=1):
+    #     print '\tNDCG@%d: %.5f' % (i, score) 
+    print 'Karma MSE: %.5f' % mean_squared_error(test_y, test_pred)
+
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Run SVR')
+
+    parser.add_argument('datafile', type=str, help='HDF5 data file')
+
+    parser.add_argument('-f', '--features', type=str, 
+                        nargs='+', help='Features list')
+
+    parser.add_argument('-t', '--target', dest='target',
+                        type=str, default='score',
+                        help="Training objective (e.g. score)")
+
+    parser.add_argument('-l', '--list-features', action='store_true', default=False,
+                        help='Show possible features', dest='list_features')
+
+    parser.add_argument('-L', '--limit-data', dest='limit_data', 
+                        default=0, type=int,
+                        help="Limit to # of submissions")
+
+    parser.add_argument('--tf', dest='test_fraction', 
+                        default=0.9, type=float,
+                        help="Fraction to reserve for testing")
+
+    args = parser.parse_args()
+
+
+    # main(args)
+
+    import pdb
+    try:
+        main(args)
+    except Exception as e:
+        print repr(e)
+        pdb.post_mortem()
