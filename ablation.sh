eb8cd1dd2d7035897674d20f7167f611d213b63e
Sammy Nguyen <snguyen@comprehend.com>
2015-02-15 16:33:49 -0800

    Ablation script

---
+++ ablation.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DATA=$1
+TARGET="score"
+MODEL="rf"
+
+python classifier.py $DATA -t $TARGET -c $MODEL -s all_abl --fg all-june
+
+python classifier.py $DATA -t $TARGET -c $MODEL -s combo_abl --fg combo
+
+python classifier.py $DATA -t $TARGET -c $MODEL -s all_text_abl --fg all_text
+
+python classifier.py $DATA -t $TARGET -c $MODEL -s user_abl --fg user_only
+
+python classifier.py $DATA -t $TARGET -c $MODEL -s base_meta_ctxt_abl --fg baseline metadata_only context
+
+python classifier.py $DATA -t $TARGET -c $MODEL -s base_meta_abl --fg baseline metadata_only
+
+python classifier.py $DATA -t $TARGET -c $MODEL -s base_abl --fg baseline
+
+python classifier.py $DATA -t $TARGET -c $MODEL -s dummy_abl -f position_rank
