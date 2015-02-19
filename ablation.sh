ad072863d7a5615f138dc90e57d3a2d418331da4
Ian Tenney <iftenney@stanford.edu>
2015-02-18 19:53:36 -0800

    Updated ablation test for feb. 16

--- ablation.sh
+++ ablation.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
+HERE=$(dirname $0)
 DATA=$1
-TARGET="score"
+TARGET="log_score"
 MODEL="rf"
+OUTDIR=$2
+mkdir -p $OUTDIR
 
-python classifier.py $DATA -t $TARGET -c $MODEL -s all_abl --fg all
+python $HERE/classifier.py $DATA -t $TARGET -c $MODEL -s $OUTDIR/all_abl --fg all
 
-python classifier.py $DATA -t $TARGET -c $MODEL -s combo_abl --fg combo
+python $HERE/classifier.py $DATA -t $TARGET -c $MODEL -s $OUTDIR/combo_abl --fg combo
 
-python classifier.py $DATA -t $TARGET -c $MODEL -s all_text_abl --fg all_text
+python $HERE/classifier.py $DATA -t $TARGET -c $MODEL -s $OUTDIR/all_text_abl --fg all_text
 
-python classifier.py $DATA -t $TARGET -c $MODEL -s user_abl --fg user_only
+python $HERE/classifier.py $DATA -t $TARGET -c $MODEL -s $OUTDIR/user_abl --fg user_only
 
-python classifier.py $DATA -t $TARGET -c $MODEL -s base_meta_ctxt_abl --fg baseline metadata context
+python $HERE/classifier.py $DATA -t $TARGET -c $MODEL -s $OUTDIR/base_meta_ctxt_abl --fg baseline metadata context
 
-python classifier.py $DATA -t $TARGET -c $MODEL -s base_meta_abl --fg baseline metadata
+python $HERE/classifier.py $DATA -t $TARGET -c $MODEL -s $OUTDIR/base_meta_abl --fg baseline metadata
 
-python classifier.py $DATA -t $TARGET -c $MODEL -s base_abl --fg baseline
+python $HERE/classifier.py $DATA -t $TARGET -c $MODEL -s $OUTDIR/base_abl --fg baseline
 
-python classifier.py $DATA -t $TARGET -c $MODEL -s dummy_abl -f position_rank
+python $HERE/classifier.py $DATA -t $TARGET -c $MODEL -s $OUTDIR/dummy_abl -f position_rank
