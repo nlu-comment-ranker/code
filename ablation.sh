f06be867c550aa49f8a7a8a32539e3a85430f524
Ian Tenney <iftenney@stanford.edu>
2015-02-24 21:58:03 -0800

    Changed feature sets for final paper

--- ablation.sh
+++ ablation.sh
@@ -13,15 +13,15 @@ python $HERE/classifier.py $DATA --stock -t $TARGET -c $MODEL -s $OUTDIR/all_abl
 
 python $HERE/classifier.py $DATA --stock -t $TARGET -c $MODEL -s $OUTDIR/all_notd_abl --fg all-notimedelta
 
-python $HERE/classifier.py $DATA --stock -t $TARGET -c $MODEL -s $OUTDIR/top2_abl --f timedelta user_local_comment_avg_net_karma
+# python $HERE/classifier.py $DATA --stock -t $TARGET -c $MODEL -s $OUTDIR/top2_abl --f timedelta user_local_comment_avg_net_karma
 
-python $HERE/classifier.py $DATA --stock -t $TARGET -c $MODEL -s $OUTDIR/combo_abl --fg combo
+# python $HERE/classifier.py $DATA --stock -t $TARGET -c $MODEL -s $OUTDIR/combo_abl --fg combo
 
 python $HERE/classifier.py $DATA --stock -t $TARGET -c $MODEL -s $OUTDIR/all_text_abl --fg all_text
 
 python $HERE/classifier.py $DATA --stock -t $TARGET -c $MODEL -s $OUTDIR/user_abl --fg user_only
 
-python $HERE/classifier.py $DATA --stock -t $TARGET -c $MODEL -s $OUTDIR/base_meta_ctxt_abl --fg baseline metadata context
+# python $HERE/classifier.py $DATA --stock -t $TARGET -c $MODEL -s $OUTDIR/base_meta_ctxt_abl --fg baseline metadata context
 
 python $HERE/classifier.py $DATA --stock -t $TARGET -c $MODEL -s $OUTDIR/base_meta_abl --fg baseline metadata
 
